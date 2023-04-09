f6870c37a47d9e8469b305b0b6ea852839b95b68
Deepankar Singh <singh.deepankar39@gmail.com>
2023-04-09 13:25:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [busLines, setBusLines] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://3.22.116.126:9091/api/v1/bus/services');
+      const data = await response.json();
+      setBusLines(data.map(busLine => ({ ...busLine, showAllStops: false })));
+    };
+
+    fetchData();
+
+    const refreshInterval = setTimeout(() => {
+      window.location.reload();
+    }, 5 * 60 * 1000);
+
+    return () => {
+      clearTimeout(refreshInterval);
+    };
+  }, []);
+
+  const toggleShowAllStops = (index) => {
+    setBusLines(busLines.map((line, i) => {
+      if (i === index) {
+        return { ...line, showAllStops: !line.showAllStops };
+      }
+      return line;
+    }));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1><span className="heading-text">Top 10 BusLines and its BusStops</span></h1>
+      <div className="cards">
+        {busLines.map((line, index) => (
+          <div key={index} className="card">
+            <h3><span className="heading-text">Bus Line Number</span> : {line.busLineName}</h3>
+            
+            <ul>
+              <h4><span className="heading-text">Bus Stop Names</span></h4>
+              {(line.showAllStops ? line.busStopNames : line.busStopNames.slice(0, 10)).map((stop, index) => (
+                <li key={index}>{stop}</li>
+              ))}
+            </ul>
+            <button onClick={() => toggleShowAllStops(index)}>
+              {line.showAllStops ? 'Show Less' : 'Show More'}
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
